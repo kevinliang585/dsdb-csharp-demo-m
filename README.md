292fb1b0d3140e11284ca4a3b319067945c2ece3
kevin <kevin.liang@dasudian.com>
2017-09-21 10:57:39 +0800

    发布第一个版本

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # dsdb-csharp-demo-m
+
+## 介绍
+
+该项目为大数点工业大数据分析和设备管理服务的数据分析端的C# SDK的Demo程序，其中包含了SDK库文件本身和一个使用该SDK的Demo程序，演示如何通过集成大数点DSDB csharp SDK实现设备数据的分析
+
+大数点工业大数据服务让设备变得有生命，并让其整个生命周期为你管理，让她的每一次呼吸（产生数据）在大数点云端为你变现！关于如何获得大数点工业大数据分析服务，请联系大数点顾问团队sales@dasudian.com或support@dasudian.com，我们将竭诚为您展示大数点丰富多彩而致力于解决实际问题的数据分析服务。
+
+# 目录结构说明
+
+## demo目录
+
+如何使用C# SDK的客户端
+请修改demo中的user，passwd，remoteAddress，port，authDatabaseName
+
+## lib目录
+该目录下有最新的SDK库，支持.NET Framework 4.5及以上版本。
+
+## docs目录
+
+该目录下有关于SDK API的文档。
+
+# 编译和运行
+
+demo程序基于Visual Studio 2013，.NET Framework 4.5。
+
+使用Visual Studio创建新项目，导入.dll文件和demo编译运行即可。
+
---
+++ demo/Demo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using com.dasudian.sdk.dsdb;
+using MongoDB.Bson;
+using System.Threading;
+
+
+namespace mongo_csharp_test
+{
+    public class Demo
+    {
+        public static void Main(string[] args)
+        {
+            String user = "tester";
+            String passwd = "testerPasswd";
+            String remoteAddress = "192.168.1.31";
+            int port = 27017;
+            String authDatabaseName = "test";
+
+            DsdbClient client = new DsdbClient.Builder(user, passwd, remoteAddress, port, authDatabaseName).Build();
+            client.connect();
+
+            // 插入数据    2017/9/15 17:55:54 DateTime.Now.ToString()
+            
+            //BsonDocument doc = new BsonDocument();
+            //doc.Add("name", "今天吗");
+            //doc.Add("age", 26);
+            //doc.Add("nums", 24);
+            //doc.Add("time", DateTime.Now.ToString());
+            //client.put(authDatabaseName, "kevindemo0920", doc);
+
+            // 以下为查询    
+
+            DsdbSearchOption searchOption = new DsdbSearchOption();
+            searchOption.condition =
+                DsdbFilter.And(DsdbFilter.Eq("age", 25), DsdbFilter.Eq("name", "kevin111"));
+
+            Dictionary<int, String> sortMap = new Dictionary<int, String>();
+            //1 升序 ,-1 降序
+            sortMap.Add(1, "age");
+     //       sortMap.Add(-1, "time");
+
+            searchOption.sort = sortMap;
+
+            searchOption.rows = 5;
+     //       searchOption.start = 2;
+            String[] str = { "name", "age" };
+            searchOption.returnFields = str;
+
+            DsdbSearchResult dsdbSearchResult = client.search("test", "kevindemo0920", searchOption);
+            foreach (BsonDocument item in dsdbSearchResult.results)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+           /*以下为更新*/
+           
+            //DsdbUpdateDefinitionBuilder dudb = new DsdbUpdateDefinitionBuilder();
+            //client.updateOne("test", "student", DsdbFilter.And(DsdbFilter.Eq("count", 6), DsdbFilter.Eq("type", "Database")), dudb.Set("type", "kevin"));
+            //client.updateMany("test", "kevindemo0920", DsdbFilter.Lt("age", 25), dudb.Set("type", "kevin02"));
+
+
+            // 以下为删除 
+            //client.deleteMany("test", "student", DsdbFilter.Eq("qty", 45));
+
+            Thread.Sleep(10000);
+
+        }
+    }
+}
